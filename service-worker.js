c227b57f5fb23bd5c0ca1412b4056aaa5a386c9e
mrs433d <154293128+mrs433d@users.noreply.github.com>
2025-05-11 17:27:14 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,15 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker installed');
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("location-cache").then(function(cache) {
+      return cache.addAll(["/", "/index.php"]);
+    })
+  );
+});
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
